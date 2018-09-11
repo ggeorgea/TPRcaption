2fa3715ade8e8e3459f2a5b3bd96375a3d26f49c
george <ggeorge.alexander@gmail.com>
2018-09-11 15:48:00 +0100

    no message

--- TPRmodel.py
+++ TPRmodel.py
@@ -23,34 +23,113 @@ class EncoderCNN(nn.Module):
     
     
 class DecoderRNN(nn.Module):
-    def __init__(self, vocab_size, embed_size=256, hidden_size=512,num_layers=2):
+    def __init__(self, vocab_size, embed_size=256, hidden_size=512,num_layers=1):
         super(DecoderRNN, self).__init__()
         self.embed_size = embed_size
         self.hidden_size = hidden_size
         self.vocab_size = vocab_size
         self.num_layers = num_layers
-        self.lstmS = nn.LSTM(embed_size, hidden_size,num_layers,batch_first = True,dropout = 0.4)
-        self.lstmU = nn.LSTM(embed_size, hidden_size,num_layers,batch_first = True,dropout = 0.4)
+        self.lstmS = nn.LSTM(embed_size+hidden_size, hidden_size,num_layers,batch_first = True,dropout = 0.4)
+        self.lstmU = nn.LSTM(embed_size+hidden_size, hidden_size,num_layers,batch_first = True,dropout = 0.4)
         self.drop = nn.Dropout()
         self.embed_captions = torch.nn.Embedding(vocab_size, embed_size)
         self.fc = nn.Linear(hidden_size, vocab_size)
+        self.SzeroLinear = nn.Linear(embed_size,hidden_size)
     
     def forward(self, features, captions):
         self.batch_size = features.shape[0]
         #captions are batch, length
         #features are batch, length
+        #\/\/\/\/!!!!
         embedded_captions = self.embed_captions(  torch.tensor( (captions.float()).view(captions.size()[0],captions.size()[1]), dtype=torch.long).cuda()    )
-        inputS = torch.cat((features.view(features.size()[0],1,features.size()[1]),embedded_captions),dim = 1)
-        lstmS_out, self.hidden = self.lstmS(inputS)#, self.hidden)
+        # inputS = torch.cat((features.view(features.size()[0],1,features.size()[1]),embedded_captions),dim = 1)
+        inputS = features
+        #.view(1,features.size()[0],features.size()[1])
+
+        #lstmS_out, self.hidden = self.lstmS(inputS)#, self.hidden)
+        #=====================================================
+        #start word
+        lastword = self.embed_captions(torch.tensor((torch.zeros(1)), dtype=torch.long).cuda())
+        
+        lastword = lastword.view(1,1,lastword.shape[len(lastword.shape)-1])
+
+
+        wordstart = lastword.repeat(self.batch_size,1,1)#1,x,1
+        p0zeroes = torch.zeros(self.batch_size,1,self.hidden_size).cuda()#1,x,x
+        fSIn = torch.cat((p0zeroes, wordstart),dim = 2)
+        sStartHidden = (self.SzeroLinear(features)).view(self.num_layers,self.batch_size,self.hidden_size)#
+        fUin = torch.cat((p0zeroes, wordstart),dim = 2)
+        print(sStartHidden.shape)
+        lstSOut,self.hidden = self.lstmS(fSIn,sStartHidden)
+        lstUOut,self.hiddenU = self.lstmU(fUin)
+
+        #temp
+        lstmS_out.append(wordstart)
+
+        lstmS_out = []
+        lstmU_out = []
+
+        # #first attempt at first go over
+        # lstUOut =  torch.zeros(1,self.batch_size,self.hidden_size).cuda()
+        # lstmS_out = []
+        # iStartS = inputS.view(-1,inputS.size()[0],inputS.size()[1])
+        # # lstSOut =  torch.zeros(iStartS.size()[0],iStartS.size()[1],self.hidden_size).cuda()
+        # lstSOut =  torch.zeros(iStartS.size()[0],iStartS.size()[1],self.hidden_size).cuda()
+        # fSIn = torch.cat((iStartS,lstSOut ),dim = 2)
+        # #is features, empty hidden state
+        # #should be startword, empty hidden state
+        # #should be lstm0(input = startchar, hidden = linear of features to hiddensize)!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        # #TODO TODO TODO TODO
+        # lstSOut,self.hidden = self.lstmS(fSIn)
+        # lstmS_out.append(lstSOut)
+        # #p0 zero vector
+        # lstmU_out = []
+        # fUin = fSin # should change ;ater
+        # lstUOut,self.hiddenU = self.lstmU(fUin)
+        # lstmU_out.append(lstUOut)
+
+
+
+        for inty in range(1,captions.size()[1]):
+            #this should be the * of lstuout and this
+            finalO  = lstSOut
+            inp = finalO
+            #inputS[inty]
+           
+            sIn = torch.cat((inp.view(-1,inp.size()[0],inp.size()[1]),lstUOut ),dim = 2)
+            lstSOut,self.hidden = self.lstmS(sIn,self.hidden)
+            lstmS_out.append(lstSOut)
+
+            uIn = torch.cat((inp.view(-1,inp.size()[0],inp.size()[1]),lstSOut ),dim = 2)
+            lstUOut,self.hiddenU = self.lstmU(uIn,self.hiddenU)
+            lstmU_out.append(lstUOut)
+
+        lstmS_out = torch.stack(lstmS_out)
+        lstmS_out = lstmS_out.view(inputS.size()[0],inputS.size()[1],self.hidden_size)
+        lstmU_out = torch.stack(lstmU_out)
+        lstmU_out = lstmU_out.view(inputS.size()[0],inputS.size()[1],self.hidden_size)
+
+
+
+        #======================================================
         lstmS_out = self.drop(lstmS_out)
         lstmS_out = lstmS_out[:,:-1,:]
         tag_outputs = self.fc(lstmS_out)
+
+
         return tag_outputs
 
     def sample(self, inputs, states=None, max_len=20):
         " accepts pre-processed image tensor (inputs) and returns predicted sentence (list of tensor ids of length max_len) "
         inputS = inputs
+        
+
+        #TODO change to reflect above
         lstmS_out, self.hidden = self.lstmS(inputS)#, self.hidden)
+
+
+
+
         captionList = []
         lastword = self.embed_captions(torch.tensor((torch.zeros(1)), dtype=torch.long).cuda())
         for i in range (1,max_len):
--- fulltrainer.py
+++ fulltrainer.py
@@ -8,72 +8,73 @@ import torch.utils.data as data
 from pycocotools.coco import COCO
 from COCOClass import CoCoDataset
 from torchvision import transforms
-from model import EncoderCNN, DecoderRNN
+from TPRmodel import EncoderCNN, DecoderRNN
+def train():
+    vocab_from_file = True    
+    num_epochs = 2        
+    transform_train = transforms.Compose([ transforms.Resize(256), transforms.RandomCrop(224),                      
+        transforms.RandomHorizontalFlip(), transforms.ToTensor(), 
+        transforms.Normalize((0.485, 0.456, 0.406),(0.229, 0.224, 0.225))])
+    batch_size = 100         # 300
+    vocab_threshold = 5       # 5
+    img_folder = './cocoapi/images/train2014/'
+    annotations_file = './cocoapi/annotations/captions_train2014.json'
+    mode='train'
+    vocab_file='./vocab.pkl'
+    start_word="<start>"
+    end_word="<end>"
+    unk_word="<unk>"
+    dataset = CoCoDataset(transform=transform_train, mode='train', batch_size=batch_size, vocab_threshold=vocab_threshold,
+        vocab_file=vocab_file, start_word=start_word, end_word=end_word, unk_word=unk_word, annotations_file=annotations_file,
+        vocab_from_file=vocab_from_file, img_folder=img_folder)
+    indices = dataset.get_train_indices()
+    initial_sampler = data.sampler.SubsetRandomSampler(indices=indices)
+    data_loader = data.DataLoader(dataset=dataset, num_workers=0, 
+        batch_sampler=data.sampler.BatchSampler(sampler=initial_sampler,
+        batch_size=dataset.batch_size,
+        drop_last=False))
+    vocab_size = len(data_loader.dataset.vocab)
+    encoder = EncoderCNN()
+    decoder = DecoderRNN(vocab_size)
+    # encoder_file = "locencoder-1.pkl" 
+    # decoder_file = "locdecoder-1.pkl"
+    # encoder.load_state_dict(torch.load(os.path.join('./models', encoder_file)))
+    # decoder.load_state_dict(torch.load(os.path.join('./models', decoder_file)))
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    encoder.to(device)
+    decoder.to(device)
+    criterion = nn.CrossEntropyLoss().cuda() if torch.cuda.is_available() else nn.CrossEntropyLoss()
+    params = list(decoder.parameters()) + list(encoder.embed.parameters())
+    optimizer =     opt = torch.optim.Adam(params = params,lr=0.001)
+    total_step = math.ceil(len(data_loader.dataset.caption_lengths) / data_loader.batch_sampler.batch_size)
+    for epoch in range(1, num_epochs+1):
+        for i_step in range(1, total_step+1):        
+            indices = data_loader.dataset.get_train_indices()
+            new_sampler = data.sampler.SubsetRandomSampler(indices=indices)
+            data_loader.batch_sampler.sampler = new_sampler
 
-vocab_from_file = True    
-num_epochs = 2        
-transform_train = transforms.Compose([ transforms.Resize(256), transforms.RandomCrop(224),                      
-    transforms.RandomHorizontalFlip(), transforms.ToTensor(), 
-    transforms.Normalize((0.485, 0.456, 0.406),(0.229, 0.224, 0.225))])
-batch_size = 100         # 300
-vocab_threshold = 5       # 5
-img_folder = './cocoapi/images/train2014/'
-annotations_file = './cocoapi/annotations/captions_train2014.json'
-mode='train'
-vocab_file='./vocab.pkl'
-start_word="<start>"
-end_word="<end>"
-unk_word="<unk>"
-dataset = CoCoDataset(transform=transform_train, mode='train', batch_size=batch_size, vocab_threshold=vocab_threshold,
-    vocab_file=vocab_file, start_word=start_word, end_word=end_word, unk_word=unk_word, annotations_file=annotations_file,
-    vocab_from_file=vocab_from_file, img_folder=img_folder)
-indices = dataset.get_train_indices()
-initial_sampler = data.sampler.SubsetRandomSampler(indices=indices)
-data_loader = data.DataLoader(dataset=dataset, num_workers=0, 
-    batch_sampler=data.sampler.BatchSampler(sampler=initial_sampler,
-    batch_size=dataset.batch_size,
-    drop_last=False))
-vocab_size = len(data_loader.dataset.vocab)
-encoder = EncoderCNN()
-decoder = DecoderRNN(vocab_size)
-# encoder_file = "locencoder-1.pkl" 
-# decoder_file = "locdecoder-1.pkl"
-# encoder.load_state_dict(torch.load(os.path.join('./models', encoder_file)))
-# decoder.load_state_dict(torch.load(os.path.join('./models', decoder_file)))
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-encoder.to(device)
-decoder.to(device)
-criterion = nn.CrossEntropyLoss().cuda() if torch.cuda.is_available() else nn.CrossEntropyLoss()
-params = list(decoder.parameters()) + list(encoder.embed.parameters())
-optimizer =     opt = torch.optim.Adam(params = params,lr=0.001)
-total_step = math.ceil(len(data_loader.dataset.caption_lengths) / data_loader.batch_sampler.batch_size)
-for epoch in range(1, num_epochs+1):
-    for i_step in range(1, total_step+1):        
-        indices = data_loader.dataset.get_train_indices()
-        new_sampler = data.sampler.SubsetRandomSampler(indices=indices)
-        data_loader.batch_sampler.sampler = new_sampler
+            images, captions = next(iter(data_loader))
+            images = images.to(device)
+            captions = captions.to(device)
 
-        images, captions = next(iter(data_loader))
-        images = images.to(device)
-        captions = captions.to(device)
-
-        decoder.zero_grad()
-        encoder.zero_grad()
-        features = encoder(images)
-        outputs = decoder(features, captions)
-        loss = criterion(outputs.view(-1, vocab_size), captions.view(-1))
-        loss.backward()
-        optimizer.step()
-        stats = 'Epoch [%d/%d], Step [%d/%d], Loss: %.4f, Perplexity: %5.4f' % (epoch, num_epochs, i_step, total_step, loss.item(), np.exp(loss.item()))
-        print('\r' + stats, end="")
-        sys.stdout.flush()
-        print('\r' + stats)
-        if i_step % 10 == 0:
+            decoder.zero_grad()
+            encoder.zero_grad()
+            features = encoder(images)
+            outputs = decoder(features, captions)
+            loss = criterion(outputs.view(-1, vocab_size), captions.view(-1))
+            loss.backward()
+            optimizer.step()
+            stats = 'Epoch [%d/%d], Step [%d/%d], Loss: %.4f, Perplexity: %5.4f' % (epoch, num_epochs, i_step, total_step, loss.item(), np.exp(loss.item()))
+            print('\r' + stats, end="")
+            sys.stdout.flush()
+            print('\r' + stats)
+            if i_step % 10 == 0:
+                torch.save(decoder.state_dict(), os.path.join('./models', 'ttdecoder-%d.pkl' % (epoch+0)))
+                torch.save(encoder.state_dict(), os.path.join('./models', 'ttencoder-%d.pkl' % (epoch+0)))
+                return
+        if epoch % 1 == 0:
             torch.save(decoder.state_dict(), os.path.join('./models', 'ttdecoder-%d.pkl' % (epoch+0)))
             torch.save(encoder.state_dict(), os.path.join('./models', 'ttencoder-%d.pkl' % (epoch+0)))
-    if epoch % 1 == 0:
-        torch.save(decoder.state_dict(), os.path.join('./models', 'ttdecoder-%d.pkl' % (epoch+0)))
-        torch.save(encoder.state_dict(), os.path.join('./models', 'ttencoder-%d.pkl' % (epoch+0)))
 
-
+train()
 
